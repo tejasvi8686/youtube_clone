f1b9b5bd3978147722a49c1764c38f7c4bcfaa4b
tejasvi8686 <tejasviraj8686@gmail.com>
2023-08-13 01:47:20 +0530

    added feed

--- src/components/Feed.js
+++ src/components/Feed.js
@@ -5,11 +5,33 @@ import LeftNav from "./LeftNav";
 import VideoCard from "./VideoCard";
 
 const Feed = () => {
-  return (
-    <div className="flex flex-row h-[cal(100%-56px)]">
-    <LeftNav/>
-    </div>
-  )
-}
+  const { loading, searchResults } = useContext(Context);
 
-export default Feed
+  useEffect(() => {
+    document.getElementById("root").classList.remove("custom-h");
+  }, []);
+
+  return (
+    <div className="flex flex-row h-[calc(100%-56px)]">
+      <LeftNav />
+      <div className="grow w-[calc(100%-240px)] h-full overflow-y-auto bg-black">
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4 p-5">
+          {/* Check if searchResults is an array before using .map() */}
+          {!loading &&
+            Array.isArray(searchResults) &&
+            searchResults.map((item) => {
+              if (item?.type !== "video") return false;
+              return (
+                <VideoCard
+                  key={item?.video?.videoId}
+                  video={item?.video}
+                />
+              );
+            })}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Feed;
